9bda14a8f51e8df8312a9ecba979a9bec0207fe7
Szabo Robert <68609388+szr2001@users.noreply.github.com>
2025-03-18 12:27:10 +0200

    Added support for github pages deployement

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
+//deploy on github pages docs
+//https://dev.to/shrinivasv73/how-to-deploy-reactjs-apps-built-using-vite-on-github-pages-38bi
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base: '/WebMouseTester/',
     server: {
         port: 54093,
     }
