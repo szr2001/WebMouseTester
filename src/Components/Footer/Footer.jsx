6dc549fa80ca6d7103a055853cc6aca920379936
Szabo Robert <68609388+szr2001@users.noreply.github.com>
2025-03-20 19:34:21 +0200

    Fixed Footer links moving when you hover over one

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,10 +5,18 @@ function Footer() {
         <div className="footer">
             <h4 className="footer-desc">Free tester for different devices!</h4>
             <div className="footer-links">
-                <a href="https://discord.gg/XYu7B4X2DQ" className="footer-link">Discord</a>
-                <a href="https://www.youtube.com/c/RoberBot" className="footer-link">Youtube</a>
-                <a href="https://github.com/szr2001" className="footer-link">Github</a>
-                <a href="https://www.linkedin.com/in/roberbot/" className="footer-link">Linkedin</a>
+                <div className="footer-link">
+                    <a href="https://discord.gg/XYu7B4X2DQ" className="footer-link">Discord</a>
+                </div>
+                <div className="footer-link">
+                    <a href="https://www.youtube.com/c/RoberBot" className="footer-link">Youtube</a>
+                </div>
+                <div className="footer-link">
+                    <a href="https://github.com/szr2001" className="footer-link">Github</a>
+                </div>
+                <div className="footer-link">
+                    <a href="https://www.linkedin.com/in/roberbot/" className="footer-link">Linkedin</a>
+                </div>
             </div>
       </div>
   );
